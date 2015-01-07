d41570ac3d98bdf5ffb37e57165a4bba56864761
Paul Walsh <paulywalsh@gmail.com>
2015-01-07 17:00:40 +1100

    Fix formatting error.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name=‘Reporter’,
+    name='Reporter',
     version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
