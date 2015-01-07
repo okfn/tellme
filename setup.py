d991ba333221982877bc82c062a3e91f1fbb6dc2
Paul Walsh <paulywalsh@gmail.com>
2015-01-07 16:48:11 +1100

    placeholder setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name=‘Reporter’,
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'PyYAML==3.11'
+    ]
+)
